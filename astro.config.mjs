21e5b98e3564cffe6966bad883a924c957c5b22b
green-spot <in.green.sopt@gmail.com>
2022-11-09 16:44:54 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://in-green-spot.com',
 	integrations: [mdx(), sitemap()],
 	server: {port: 3001}
 });
